852f77afe471951e387a612b68a1dee11ff5709a
admin <ajay.pandit@shoptimize.in>
2020-12-14 13:45:00 +0530

    removing tensorflow

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
-from flask import Flask, render_template, request
-from scipy.misc import imsave, imread, imresize
-import numpy as np
-import keras.models
-import re
-
-import sys
+from flask import Flask, render_template, request, jsonify
+#from scipy.misc import imsave, imread, imresize
+#import numpy as np
+from lit_job_search.JobSearch import find_jobs_from
+#from PIL import Image
 import os
-from lit_digit_recognizer.model.load import init
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+import base64
+import os
+import re
+#from lit_digit_recognizer.model.load import init
 
 app = Flask(__name__)
-global model, graph
-model, graph = init()
+#global model, graph
+#model, graph = init()
+
+
+def convert_and_save(b64_string, altchars=b'+/'):
+    with open("output.png", "wb") as fh:
+        fh.write(base64.decodebytes(b64_string + b'=' * (-len(b64_string) % 4)))
+
+
+def save_as_png(canvas, fileName='output'):
+    canvas.postscript(file=fileName + '.eps')
+    # use PIL to convert to PNG
+    img = Image.open(fileName + '.eps')
+    img.save(fileName + '.png', 'png')
 
 
 def convertImage(imgData1):
-    imgstr = re.search(r'base64,(.*)', imgData1).group(1)
     with open('output.png', 'wb') as output:
-        output.write(imgstr.decode('base64'))
+        output.write(imgData1)
 
 
 @app.route('/')
@@ -25,23 +40,35 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/predict/', methods=['GET', 'POST'])
-def predict():
-    imgData = request.get_data()
-    convertImage(imgData)
-    print("debug")
-    x = imread('output.png', mode='L')
-    x = np.invert(x)
-    x = imresize(x, (28, 28))
-    x = x.reshape(1, 28, 28, 1)
-    print("debug2")
-    with graph.as_default():
-        out = model.predict(x)
-        print(out)
-        print(np.argmax(out, axis=1))
-        print("debug3")
-        response = np.array_str(np.argmax(out, axis=1))
-        return response
+@app.route('/api/v1/jobs/', methods=['GET'])
+def get_all_jobs():
+    # website = request.args.get('website')
+    title = request.args.get('title')
+    location = request.args.get('location')
+    if not title or not location:
+        return jsonify({'result': 'Please provide Job Title & Job Location'})
+    find_jobs_from('Indeed', title, location)
+    return jsonify(find_jobs_from('Indeed', title, location))
+
+
+# @app.route('/predict/', methods=['GET', 'POST'])
+# def predict():
+#     imgData = request.get_data()
+#     convert_and_save(imgData)
+#     print("debug")
+#
+#     x = imread(os.path.join(here, 'output.png'), mode='L')
+#     x = np.invert(x)
+#     x = imresize(x, (28, 28))
+#     x = x.reshape(1, 28, 28, 1)
+#     print("debug2")
+#     with graph.as_default():
+#         out = model.predict(x)
+#         print(out)
+#         print(np.argmax(out, axis=1))
+#         print("debug3")
+#         response = np.array_str(np.argmax(out, axis=1))
+#         return response
 
 
 if __name__ == "__main__":
--- lit_digit_recognizer/model/load.py
+++ lit_digit_recognizer/model/load.py
@@ -12,7 +12,7 @@ def init():
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
-    loaded_model.load_weights('model.h5')
+    loaded_model.load_weights(os.path.join(here, 'model.h5'))
     print("Loaded Model from disk")
 
     # compile and evaluate loaded model
